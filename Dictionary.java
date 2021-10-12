05bde607d51d6b111ee15fedd8a802b4859591d3
talonnoxos <44602739+talonnoxos@users.noreply.github.com>
2021-10-12 22:22:10 +0700

    push thu vien dictionary

---
+++ Dictionary.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Dictionary {
+
+    final String FILE_INPUT = "D:\\oop\\Dictionary\\datadictionary.txt";//duong dan tep
+
+    public ArrayList<Word>  wordList = new ArrayList<Word>();
+
+    Dictionary() {
+
+    }
+}
